b7ae117d9448653701de30e7397b6baa5f1c3a41
Bruno Montebello <brunormontebello@hotmail.com>
2021-11-05 00:20:38 -0300

    Creates Note's actions + set user + relate Note to user

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,27 +1,56 @@
 class NotesController < ApplicationController
-  def index
+  before_action :authenticate_user!
+  before_action :set_note, only: [ :show, :edit, :update, :destroy ]
 
+  def index
+    @notes = Note.where(user_id: current_user)
   end
 
   def show; end
 
   def new
-
-  end
-
-  def create
-
+    @note = current_user.notes.build
   end
 
   def edit
+  end
 
+  def create
+    @note = current_user.notes.build(note_params)
+
+    respond_to do |format|
+      if @note.save
+        format.html { redirect_to @note, notice: "Nota criada com sucesso." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
-
+    respond_to do |format|
+      if @note.update(note_params)
+        format.html { redirect_to @note, notice: "Nota atualizada com sucesso." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
-
+    @note.destroy
+    respond_to do |format|
+      format.html { redirect_to notes_url, notice: "Nota deletada com sucesso." }
+    end
   end
+
+  private
+
+    def set_note
+      @note = Note.find(params[:id])
+    end
+
+    def note_params
+      params.require(:note).permit(:title, :content, :priority)
+    end
 end
